99fb92ee71056b83dbaabc9db9563d9ead4b4824
unknown <jwderry@L-R9ELYF6.eads.ncads.net>
2014-09-11 21:57:51 -0400

    Added chapter3 exercise

---
+++ chapter3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chapter3::Application.config.secret_token = '2dbbaea51809d15e80edd2f7acda803667539497a92748b41249629f89897e844bf03dd83683fb7bc09935dc7c9f8b36184d53e7c873a8605d22ab759ab4e812'
